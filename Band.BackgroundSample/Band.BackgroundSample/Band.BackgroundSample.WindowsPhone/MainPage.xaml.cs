57ce8d5e8130f795bee1a58e0e2cc446d86b18cc
James Croft <jamz_c@hotmail.co.uk>
2015-07-26 08:58:41 +0100

    Initial Windows Phone blank app check-in
    Created a blank Windows Phone 8.1 application to use as the base for the
    Microsoft Band background task sample

---
+++ Band.BackgroundSample/Band.BackgroundSample/Band.BackgroundSample.WindowsPhone/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="MainPage.xaml.cs" company="James Croft">
+//   Copyright (c) James Croft 2015.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Band.BackgroundSample
+{
+    using Windows.UI.Xaml.Navigation;
+
+    /// <summary>
+    /// The main page.
+    /// </summary>
+    public sealed partial class MainPage
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MainPage"/> class.
+        /// </summary>
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            this.NavigationCacheMode = NavigationCacheMode.Required;
+        }
+    }
+}
