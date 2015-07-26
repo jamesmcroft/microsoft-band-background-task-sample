0bc69935c863f2b6ff2adcf69d27e292fd756b6c
James Croft <jamz_c@hotmail.co.uk>
2015-07-26 09:15:12 +0100

    Added BG task project and nuget packages for MS Band SDK

---
+++ Band.BackgroundSample/Band.BackgroundTask/BandHealthTask.cs
@@ -0,0 +1,37 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="BandHealthTask.cs" company="James Croft">
+//   Copyright (c) James Croft 2015.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Band.BackgroundTask
+{
+    using System;
+
+    using Windows.ApplicationModel.Background;
+
+    /// <summary>
+    /// The band health background task.
+    /// </summary>
+    public sealed class BandHealthTask : IBackgroundTask, IDisposable
+    {
+        /// <summary>
+        /// Runs the background task.
+        /// </summary>
+        /// <param name="taskInstance">
+        /// The task instance responsible for running this code.
+        /// </param>
+        public void Run(IBackgroundTaskInstance taskInstance)
+        {
+
+        }
+
+        /// <summary>
+        /// Performs application-defined tasks associated with freeing, releasing, or resetting unmanaged resources.
+        /// </summary>
+        /// <filterpriority>2</filterpriority>
+        public void Dispose()
+        {
+        }
+    }
+}
